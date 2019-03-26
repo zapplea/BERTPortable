b8268988651584a589a437b7b6d08b8c0e82269b
yibing <zapplea@gmail.com>
2019-03-26 14:11:46 +0800

    add

--- run_learn.sh
+++ run_learn.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 echo "loading"
 module load python/3.6.1
 module load tensorflow/1.12.0-py36-gpu
