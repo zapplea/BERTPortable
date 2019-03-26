fe2bbbdfa3061dffda85d5719bb3f3c179be8250
yibing <zapplea@gmail.com>
2019-03-26 14:45:44 +0800

    add

--- multiGPU_graph.py
+++ multiGPU_graph.py
@@ -309,8 +309,8 @@ def gather_indexes(sequence_tensor, positions):
   width = sequence_shape[2]
 
   flat_offsets = tf.reshape(
-      tf.range(0, batch_size, dtype=tf.int64) * seq_length, [-1, 1])
-  flat_positions = tf.reshape(positions + flat_offsets, [-1])
+      tf.range(0, batch_size, dtype=tf.int32) * seq_length, [-1, 1])
+  flat_positions = tf.reshape(positions + tf.cast(flat_offsets,dtype='int64'), [-1])
   flat_sequence_tensor = tf.reshape(sequence_tensor,
                                     [batch_size * seq_length, width])
   output_tensor = tf.gather(flat_sequence_tensor, flat_positions)
