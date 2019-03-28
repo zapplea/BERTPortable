a4ca7e3707756e3f0f1b335f31ec64be5b3a91c3
yibing <zapplea@gmail.com>
2019-03-28 13:10:42 +0800

    add

--- train.py
+++ train.py
@@ -37,7 +37,10 @@ class Train:
                     self.report('data file: %s'%self.config['data']['train_dataset_filePath']%j,self.report_file,'report')
                     df = self.df(self.config,j)
                     dataset = df.dataset_generator()
+                    count = 0
                     for input_ids,input_mask,segment_ids,masked_lm_positions,masked_lm_ids,masked_lm_weights,next_sentence_labels in dataset:
+                        print('count: ',count)
+                        count+=1
                         tower_data = {'input_ids': input_ids,
                                       'input_mask': input_mask,
                                       'segment_ids': segment_ids,
